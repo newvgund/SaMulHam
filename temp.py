0fa134077317564b80f0d58f031e59a35538aaaa
Kim Jin Hoon <glaive_edge@naver.com>
2014-02-26 23:50:41 +0900

    add some test code. written by python & gi library.

---
+++ temp.py
@@ -0,0 +1,32 @@
+from gi.repository import Gtk, Gdk
+
+window = Gtk.Window()
+window.connect("destroy", Gtk.main_quit)
+
+button = Gtk.Button()
+button.set_label("Testing!")
+window.add(button)
+
+css_data = """
+GtkWindow
+{
+        background-color: #000000;
+}
+
+GtkButton
+{
+        background-color: #00FF00;
+}
+"""
+
+css_provider = Gtk.CssProvider()
+css_provider.load_from_data(css_data)
+
+screen = Gdk.Screen.get_default()
+
+style_context = Gtk.StyleContext()
+style_context.add_provider_for_screen(screen, css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+
+window.show_all()
+
+Gtk.main()
